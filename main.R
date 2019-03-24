92bb03e12d91789e618c4857fea68bc60a7fd783
Liming Cai <daybreak.chua@gmail.com>
2019-03-24 15:01:47 -0400

    Update main.R

--- main.R
+++ main.R
@@ -3,6 +3,7 @@
 #load library and relevant function
 library(phybase)
 library(ape)
+library(phytools)
 
 source('./count_triple_refq.R')
 source('./geneTr_simulator.R')
@@ -32,6 +33,12 @@ triple_writer(speciesTr,geneTr,paste(out_prefix,'_tripleFr.emp.csv',sep=''))
 ###########################################################
 #simulate gene trees under coalescent model for BP species trees
 
+#prepare dictionary of missing data for each species
+
+
+
+
+
 speciesTr_BP_text=readLines(speciesTr_BP_path)
 for (i in 1:length(speciesTr_BP_text)){
 	geneTr_sim_text=geneTr_sim(speciesTr_BP_text[i],geneTr_num)
@@ -55,4 +62,59 @@ for (i in 1:length(speciesTr_BP_text)){
 }
 
 ###########################################################
+#compare the empirical and the simulated triple frequency distribution and identify significantly unbalanced triples
+
+
+#read triple frequency and find significant unbalanced minor triple through chisq test pvalue
+#x=read.csv('total423.MLgene.triFr.csv')
+#chisq test
+#y=list()
+#for (i in 2:length(x[1,])){
+#	ordered_num=sort(as.numeric(as.character(x[4:6,i])))
+#	y=c(y,chisq.test(ordered_num,p=c((1-ordered_num[3]/sum(ordered_num))/2,(1-ordered_num[3]/sum(ordered_num))/2,ordered_num[3]/sum(ordered_num)))$p.value)
+#}
+#a=which(y<0.05)+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#summarize the percentage of unbalanced triples onto species trees
+
+a=read.csv('/Users/lcai/Downloads/Malpighiales_ILS/Triple_frequency/unbalance_minor_triples.csv')
+#read unrooted mpest species tree
+e=read.tree('/Users/lcai/Downloads/Malpighiales_ILS/coal_ana/ExonIntron/total423.mpest.mb.95cs.sum.tre')
+e$node.label=rep(0,length(e$node.label))
+
+get_nodes_leading_to_tips<-function(tree,tips){
+	tip_no=list()
+	for (tip in tips){tip_no=c(tip_no,which(tree$tip.label==tip))}
+	tip_no=unlist(tip_no)
+	nodes=list()
+	nodes=getMRCA(tree,tips)
+	for (NOD in getDescendants(tree,nodes[1])){
+		if (NOD >length(tree$tip.label) & length(intersect(tip_no,getDescendants(tree,NOD)))>0){
+			nodes=c(nodes,NOD)
+		}
+	}
+	return(nodes)
+}
+
+#
+for (i in 2:length(a[1,])){
+	e$node.label[get_nodes_leading_to_tips(e,as.character(a[1:3,i]))-65]=e$node.label[get_nodes_leading_to_tips(e,as.character(a[1:3,i]))-65]+1
+}
+
+e$edge.length=NULL
+write.tree(e,'/Users/lcai/Downloads/Malpighiales_ILS/Triple_frequency/Speciestree_unbalanced_triples.tre')
 
