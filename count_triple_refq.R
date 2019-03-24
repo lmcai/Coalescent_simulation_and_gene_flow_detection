eeb408d41cb4197e2e660f11e11bfb08f66a2ffd
Liming Cai <daybreak.chua@gmail.com>
2019-03-24 14:05:56 -0400

    Update count_triple_refq.R

--- count_triple_refq.R
+++ count_triple_refq.R
@@ -2,26 +2,12 @@
 #The required input is one species tree (newick form in one file) and all of your rooted gene trees (newick form in one file)
 #Liming Cai (lcai@g.harvard.edu)
 
-library(ape)
-
-####read in a species tree and initiate values in triple frequency
-spTr=read.tree('/Users/lcai/Downloads/Malpighiales_ILS/coal_ana/ExonIntron/total423.mpest.mb.100pp.sum.tre')
-MLgeneTr=read.tree('/Users/lcai/Downloads/Malpighiales_ILS/GeneTaxaCharacters/GenetreeBipartitions/ExonIntron.423.MLgeneTr.tre')
-
-spNam=spTr$tip.label
-ML_triples=combn(spNam,3)
-ML_triples=data.frame(ML_triples,stringsAsFactors=FALSE)
-ML_triples=rbind(ML_triples,rep(0,length(ML_triples[1,])))
-ML_triples=rbind(ML_triples,rep(0,length(ML_triples[1,])))
-ML_triples=rbind(ML_triples,rep(0,length(ML_triples[1,])))
 
 
-
-###define core function
 count_triple_treq<-function(phy,matrix){
-	for (i in 1:length(ML_triples[1,])){
-		if (all(ML_triples[1:3,i] %in% phy$tip.label)){
-			pruned.tree<-drop.tip(phy,phy$tip.label[-match(ML_triples[1:3,i], phy$tip.label)])
+	for (i in 1:length(matrix[1,])){
+		if (all(matrix[1:3,i] %in% phy$tip.label)){
+			pruned.tree<-drop.tip(phy,phy$tip.label[-match(matrix[1:3,i], phy$tip.label)])
 			if (is.monophyletic(pruned.tree,matrix[c(1,2),i])){
 				matrix[4,i]=as.numeric(matrix[4,i])+1
 				next	
@@ -39,15 +25,23 @@ count_triple_treq<-function(phy,matrix){
 	return(matrix)
 }
 
-##########calculate triple frequency for gene trees########
-for (j in 1:length(MLgeneTr)){
-	print(paste('Processing tree',j))
-	ML_triples=count_triple_treq(MLgeneTr[[j]],ML_triples)
+
+
+triple_writer<-function(speciesTr,MLgeneTr,prefix){
+	####read in a species tree and initiate values in triple frequency
+	spNam=speciesTr$tip.label
+	ML_triples=combn(spNam,3)
+	ML_triples=data.frame(ML_triples,stringsAsFactors=FALSE)
+	ML_triples=rbind(ML_triples,rep(0,length(ML_triples[1,])))
+	ML_triples=rbind(ML_triples,rep(0,length(ML_triples[1,])))
+	ML_triples=rbind(ML_triples,rep(0,length(ML_triples[1,])))
+
+	##########calculate triple frequency for gene trees########
+	for (j in 1:length(MLgeneTr)){
+		print(paste('Processing tree',j))
+		ML_triples=count_triple_treq(MLgeneTr[[j]],ML_triples)
+	}
+	write.csv(ML_triples,paste(prefix,'_triple_fr_emp.csv',sep=''))
 }
-
-###########################################################
-#examine the distribution of BS trees
-
-
 
 
