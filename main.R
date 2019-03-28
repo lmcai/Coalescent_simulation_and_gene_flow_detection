636d185d4298d716ca9d0c7c16d31d3fa6c4f337
Liming Cai <daybreak.chua@gmail.com>
2019-03-27 23:21:10 -0400

    Update main.R

--- main.R
+++ main.R
@@ -10,13 +10,16 @@ source('./geneTr_simulator.R')
 
 
 #input
-
+#species tree with branch length in coalescent units
 speciesTr_path=
 speciesTr=read.tree(speciesTr_path)
+sp_num=length(speciesTr$tip.label)
 
+#bootstrap species tree with branch length in coalescent units
 speciesTr_BP_path=
 speciesTr_BP=read.tree(speciesTr_BP_path)
 
+#gene trees
 geneTr_path=
 geneTr=read.tree(geneTr_path)
 geneTr_num=length(geneTr)
@@ -28,15 +31,20 @@ out_dir=
 #count triple frequency in empirical data
 triple_writer(speciesTr,geneTr,paste(out_dir,'/tripleFr.emp.csv',sep=''))
 
-#output triple frequency matrix written to 'out_prefix__triple_fr_emp.csv'
+#output triple frequency matrix written to 'out_dir/tripleFr.emp.csv'
 
 ###########################################################
 #simulate gene trees under coalescent model for BP species trees
 
-#prepare dictionary of missing data for each species
-
-
-
+#prepare list of missing data for each species
+sp_missing=list()
+#initiate
+for (sp in speciesTr$tip.label){sp_missing[sp]=0}
+for (sp in speciesTr$tip.label){
+	for (tr in geneTr){
+		if (sp %in% tr$tip.label){sp_missing[sp]=as.numeric(sp_missing[sp])+1}
+	}
+}
 
 
 speciesTr_BP_text=readLines(speciesTr_BP_path)
@@ -45,13 +53,13 @@ for (i in 1:length(speciesTr_BP_text)){
 	simulated_gene_trees=read.tree(geneTr_sim_text)
 	
 	#simulate missing data according to empirical data
-	
-	
-	
-	
-	
-	
-	
+	for (sp in speciesTr$tip.label){
+		num_missing=geneTr_num-as.numeric(sp_missing[sp])
+		tr2prune=sample(1:geneTr_num,num_missing)
+		for (j in tr2prune){
+			simulated_gene_trees[[j]]=drop.tip(simulated_gene_trees[[j]],sp)
+		}
+	}
 	
 	
 	#output to file
@@ -66,19 +74,18 @@ for (i in 1:length(speciesTr_BP_text)){
 
 tripleFr_outlier_detect
 
+outliers
 
 
 
 
 
+#output to csv
+write.csv(outliers,paste(out_dir,'/unbalanced_triples.csv',sep=''))
 
-
-
-
-
+###########################################################
 #summarize the percentage of unbalanced triples onto species trees
 
-a=read.csv('/Users/lcai/Downloads/Malpighiales_ILS/Triple_frequency/unbalance_minor_triples.csv')
 #read unrooted mpest species tree
 e=read.tree('/Users/lcai/Downloads/Malpighiales_ILS/coal_ana/ExonIntron/total423.mpest.mb.95cs.sum.tre')
 e$node.label=rep(0,length(e$node.label))
@@ -97,11 +104,10 @@ get_nodes_leading_to_tips<-function(tree,tips){
 	return(nodes)
 }
 
-#
-for (i in 2:length(a[1,])){
-	e$node.label[get_nodes_leading_to_tips(e,as.character(a[1:3,i]))-65]=e$node.label[get_nodes_leading_to_tips(e,as.character(a[1:3,i]))-65]+1
+#count raw numbers of unbalanced triples for each node
+for (i in 2:length(outliers[1,])){
+	e$node.label[get_nodes_leading_to_tips(e,as.character(outliers[1:3,i]))-sp_num]=e$node.label[get_nodes_leading_to_tips(e,as.character(outliers[1:3,i]))-sp_num]+1
 }
-
 e$edge.length=NULL
-write.tree(e,'/Users/lcai/Downloads/Malpighiales_ILS/Triple_frequency/Speciestree_unbalanced_triples.tre')
+write.tree(e,paste(out_dir,'/unbalanced_triples_sum.tre',sep=''))
 
