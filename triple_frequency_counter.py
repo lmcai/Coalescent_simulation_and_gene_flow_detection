160e5f7bdecb978e3d51780a269cfee700162478
Liming Cai <34404264+lmcai@users.noreply.github.com>
2020-07-07 21:55:28 -0400

    Update triple_frequency_counter.py

--- triple_frequency_counter.py
+++ triple_frequency_counter.py
@@ -1,13 +1,11 @@
 from ete3 import Tree
 import itertools
-
+import sys
 
 #set multiple monophyletic outgroups to allow for missing data
-outgroup1=["Uronema_sp", "Nephroselmis_pyriformis", "Pyramimonas_parkeae", "Monomastix_opisthostigma"]
-outgroup2=["Spirogyra_sp","Chlorokybus_atmophyticus","Mesotaenium_endlicherianum"]
 
-geneTr_text=open('./Fig3.424.gene.trees').readlines()
-spTr_text=open('./Fig3.rooted.tre').readline()
+geneTr_text=open(sys.argv[1]).readlines()
+spTr_text=open(sys.argv[2]).readline()
 
 spTr = Tree(spTr_text.strip())
 total_taxon=list()
@@ -28,77 +26,31 @@ for i in range(0,len(geneTr_text)):
 	t = Tree(geneTr_text[i].strip())
 	taxon=list()
 	for leaf in t:taxon.append(leaf.name)
-	
-		
 	###########################################
 	#gene tree rooting	
-	#must have at least one outgroup
-	if any(l in taxon for l in outgroup1):
-		#outgroup in gene tree
-		geneTr_out=[l for l in outgroup1 if l in taxon]
-		if len(geneTr_out)==1:
-			t.set_outgroup( t&geneTr_out[0])
-		elif t.check_monophyly(values=geneTr_out, target_attr="name")[0]:
-			ancestor = t.get_common_ancestor(geneTr_out)
-			t.set_outgroup(ancestor)
-		else:
-			#if outgroups are paraphyletic, use the monophyletic clade with largest number of outgroup species
-			#raise ValueError("outgroup is paraphyletic")
-			max_out=1
-			print '\tparaphyletic outgroup in tree' +`i`
-			#annotate outgroup species
-			for leaf in t:
-				if leaf.name in outgroup1:
-					#print leaf.name
-					leaf.add_features(outgr='true')
-			
-			for node in t.get_monophyletic(values=["true"], target_attr="outgr"):
-				if len(node)>=max_out:
-					t.set_outgroup(node)
-					max_out=len(node)
-			
-	#start to search for second outgroup clades		
-	elif any(l in taxon for l in outgroup2):
-		#outgroup in gene tree
-		geneTr_out=[l for l in outgroup2 if l in taxon]
-		if len(geneTr_out)==1:
-			t.set_outgroup( t&geneTr_out[0])
-		elif t.check_monophyly(values=geneTr_out, target_attr="name")[0]:
-			ancestor = t.get_common_ancestor(geneTr_out)
-			t.set_outgroup(ancestor)
-		else:
-			#if outgroups are paraphyletic, use the monophyletic clade with largest number of outgroup species
-			#raise ValueError("outgroup is paraphyletic")
-			max_out=1
-			print '\tparaphyletic outgroup in tree' +`i`
-			#annotate outgroup species
-			for leaf in t:
-				if leaf.name in outgroup2:
-					#print leaf.name
-					leaf.add_features(outgr='true')
-			
-			for node in t.get_monophyletic(values=["true"], target_attr="outgr"):
-				if len(node)>=max_out:
-					t.set_outgroup(node)
-					max_out=len(node)
-	
-	
-	else:
-		print '\tno outgroup in tree'+`i`
-		continue 
-
+	#ancestor=t.get_common_ancestor('Clus','Cdub')
+	try:
+	#	t.set_outgroup(ancestor)
 	###########################################
 	#triple counting
-	triples=list(itertools.combinations(taxon, 3))
+		triples=list(itertools.combinations(taxon, 3))
 	
-	for j in range(0,len(triples)):
-		tpl_taxa=sorted(triples[j])
-		if t.get_common_ancestor(tpl_taxa[0],tpl_taxa[2])==t.get_common_ancestor(tpl_taxa[1],tpl_taxa[2]):
-			triple_dict['|'.join(tpl_taxa)][0]+=1
-		elif t.get_common_ancestor(tpl_taxa[0],tpl_taxa[1])==t.get_common_ancestor(tpl_taxa[1],tpl_taxa[2]):
-			triple_dict['|'.join(tpl_taxa)][1]+=1
-		else:
-			triple_dict['|'.join(tpl_taxa)][2]+=1
+		for j in range(0,len(triples)):
+			tpl_taxa=sorted(triples[j])
+			if t.get_common_ancestor(tpl_taxa[0],tpl_taxa[2])==t.get_common_ancestor(tpl_taxa[1],tpl_taxa[2]):
+				triple_dict['|'.join(tpl_taxa)][0]+=1
+			elif t.get_common_ancestor(tpl_taxa[0],tpl_taxa[1])==t.get_common_ancestor(tpl_taxa[1],tpl_taxa[2]):
+				triple_dict['|'.join(tpl_taxa)][1]+=1
+			else:
+				triple_dict['|'.join(tpl_taxa)][2]+=1
+	except:
+		print('bad rooting')
+		pass
+
+out=open(sys.argv[1].split('.')[0]+'.trp.tsv','a')
 
 for k, v in triple_dict.iteritems():
-	print k, v
+	out.write(k+'\t'+'\t'.join([str(l) for l in v])+'\n')
+
+out.close()
+
