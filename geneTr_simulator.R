3efcd1a8290fd425238727e2fe7c495c345a2dc1
Liming Cai <34404264+lmcai@users.noreply.github.com>
2020-07-08 10:42:57 -0400

    Delete geneTr_simulator.R

--- geneTr_simulator.R
+++
@@ -1,7 +0,0 @@
-#take in one rooted species tree text with branch length in coalescent units and simulate gene trees
-#output gene trees in text
-
-geneTr_sim<-function(SpTr_text,number_geneTr){
-	y=sim.coal.mpest(SpTr_text,number_geneTr)
-	return(y)
-}
