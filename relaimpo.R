f991131d9d937e65a43d1fd75f8e21180ebfa8c3
Liming Cai <34404264+lmcai@users.noreply.github.com>
2020-09-29 22:59:04 -0700

    Update relaimpo.R

--- relaimpo.R
+++ relaimpo.R
@@ -1,14 +1,11 @@
 library(relaimpo)
 #This R script conducts relative importance decomposition to assign shares of importance of each variable and plot the results.
 
-data=read.csv('./example/relative_contribution_ILS_Err_Intro.csv')
+x=read.csv('./example/relative_contribution_ILS_Err_Intro.csv')
+data=x[,c('Y_BSsum','X1_ILS','X2_Err','X3_Hyb')]
 bootimpo.result <- boot.relimp(data, b = 100,
                     type = c("lmg", "last", "first", "pratt"),
                     rank = TRUE, diff = TRUE, rela = TRUE)
 
-plot(bootimpo.result)
-
-## function for evaluating bootstrap results
-booteval.relimp(bootimpo.result, bty = "perc", level = 0.95, 
-	sort = FALSE, norank = FALSE, nodiff = FALSE,
-	typesel = c("lmg", "last", "first", "pratt"))
+## Plot
+plot(booteval.relimp(bootimpo.result))
